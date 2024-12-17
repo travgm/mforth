f709e7a181229b130a3186c51cd4a603d83e1e77
Travis <travis@travgm.org>
2024-12-17 00:12:13 -0700

    Workign on the parse_line function, evaluation, stack operations and some error handling

--- mforth.ml
+++ mforth.ml
@@ -5,11 +5,10 @@
     Note:
     ❯ ocamlopt --version
       5.2.0+jst
-    
-    Based off of the sector forth implementation: https://github.com/cesarblum/sectorforth
 
     minimaForth (C) Copyright 2024 Travis Montoya *)
 open Base
+
 open Stdio
 
 let version = "1.0.0"
@@ -85,6 +84,7 @@ module Stack = struct
     | x :: _ -> Some x
   ;;
 
+  let is_empty (stack : t) : bool = List.is_empty stack.items
   let size (stack : t) : int = List.length stack.items
 
   let print (stack : t) : unit =
@@ -108,6 +108,19 @@ module Stack = struct
       print_items 0 stack.items)
   ;;
 
+  let pop_and_print (stack : t) =
+    match pop stack with
+    | None -> stack
+    | Some (x, stack1) ->
+      (match x with
+       | Int x -> print_endline (Int.to_string x)
+       | Float x -> print_endline (Float.to_string x)
+       | Char x -> print_endline (String.make 1 x)
+       | String x -> print_endline x
+       | Bool x -> print_endline (Bool.to_string x));
+      stack1
+  ;;
+
   let wrap_stack_item (item : string) : stack_item option =
     match Int.of_string_opt item with
     | Some n -> Some (Int n)
@@ -126,16 +139,23 @@ module Stack = struct
 end
 
 module DataState = struct
+  module StringMap = Map.M (String)
+
   type data_areas =
     { memory : Memory.t
     ; data_stack : Stack.t
     ; return_stack : Stack.t
+    ; dictionary : string StringMap.t
     }
 
   type t = { data : data_areas }
 
   let init_data () =
-    { memory = Memory.empty; data_stack = Stack.empty; return_stack = Stack.empty }
+    { memory = Memory.empty
+    ; data_stack = Stack.empty
+    ; return_stack = Stack.empty
+    ; dictionary = Map.empty (module String)
+    }
   ;;
 end
 
@@ -148,43 +168,86 @@ type func_record =
   ; tokens : string list
   }
 
+let eval_stack_op (d : DataState.data_areas) ~(op : string) : DataState.data_areas =
+  let module S = Stack in
+  let apply_op () =
+    match op with
+    | "+" -> ( + ), ( +. )
+    | "-" -> ( - ), ( -. )
+    | "*" -> ( * ), ( *. )
+    | "/" -> ( / ), ( /. )
+    | _ -> ( + ), ( +. )
+  in
+  match S.pop d.data_stack with
+  | None -> d
+  | Some (x, stack1) ->
+    (match S.pop stack1 with
+     | None -> d
+     | Some (y, stack2) ->
+       let int_op, float_op = apply_op () in
+       let result =
+         match x, y with
+         | S.Int x, S.Int y -> S.Int (int_op x y)
+         | S.Int x, S.Float y -> S.Float (float_op (Float.of_int x) y)
+         | S.Float x, S.Int y -> S.Float (float_op x (Float.of_int y))
+         | S.Float x, S.Float y -> S.Float (float_op x y)
+         | _ ->
+           print_endline "Invalid operation, expecting a numeric";
+           S.Int 0
+       in
+       let new_stack = S.push result ~stack:stack2 in
+       { d with data_stack = new_stack })
+;;
+
+let parse_function (line : string) : func_record option =
+  let words = String.split_on_chars line ~on:[ ' ' ] in
+  match words with
+  | [] -> None
+  | name :: tokens -> Some { name; tokens }
+;;
+
+(* Main line parser and REPL *)
+
 let parse_line (line : string) ~(d : DataState.data_areas) =
   let module S = Stack in
   match line with
-  (* Built-ins are compared here compared to dictionary functions *)
+  (* Built-ins NOT built-in functions are compared here compared to dictionary functions or built-in functions *)
   | ".s" ->
     S.print d.data_stack;
     d
   | ".r" ->
     S.print d.return_stack;
     d
-  (* This might need to be moved to dictionary functions? depending how we compare those *)
-  | "+" | "-" | "*" | "/" ->
-    (match S.pop d.data_stack with
-     | None -> d
-     | Some (x, stack1) ->
-       (match S.pop stack1 with
-        | None -> d
-        | Some (y, stack2) ->
-          let result =
-            match x, y with
-            | S.Int x, S.Int y -> S.Int (x + y)
-            | S.Int x, S.Float y -> S.Float (Float.of_int x +. y)
-            | S.Float x, S.Int y -> S.Float (x +. Float.of_int y)
-            | S.Float x, S.Float y -> S.Float (x +. y)
-            | _ -> S.Int 0
-          in
-          let new_stack = S.push result ~stack:stack2 in
-          { d with data_stack = new_stack }))
+  | "." ->
+    if S.is_empty d.data_stack then (
+      print_endline "Stack underflow";
+      d
+    ) else (
+      let new_stack = S.pop_and_print d.data_stack in
+      { d with data_stack = new_stack } )
+  | ("+" | "-" | "*" | "/") as op -> eval_stack_op d ~op
+  (* Eventually need to break this out more into smaller functions. This is where we parse out 
+     stack operations, function definitions and execution? *)
   | item ->
-    (match S.wrap_stack_item (String.strip item) with
-     | Some stack_item ->
-       let new_stack = S.push stack_item ~stack:d.data_stack in
-       let new_data_state = { d with data_stack = new_stack } in
-       new_data_state
-     | None ->
-       print_endline ("unknown command: " ^ item);
-       d)
+    if String.is_prefix item ~prefix:"\"" && String.is_suffix item ~suffix:"\""
+    then (
+      let cleaned_str = String.sub item ~pos:1 ~len:(String.length item - 2) in
+      let new_stack = S.push (S.String cleaned_str) ~stack:d.data_stack in
+      { d with data_stack = new_stack })
+    else (
+      match Int.of_string_opt item with
+      | Some n ->
+        let new_stack = S.push (S.Int n) ~stack:d.data_stack in
+        { d with data_stack = new_stack }
+      | None ->
+        (match Float.of_string_opt item with
+         | Some f ->
+           let new_stack = S.push (S.Float f) ~stack:d.data_stack in
+           { d with data_stack = new_stack }
+         | None ->
+           (* This is probably where we should handle testing if its a function to execute *)
+           print_endline ("unknown command: " ^ item);
+           d))
 ;;
 
 let () =
